d60505b7a28d5b564b75e99b0ec23538632f2a14
Adam <adam.l.steinberg@me.com>
2025-12-08 12:57:06 -0500

    day 6 cleanup

--- src/day6.rs
+++ src/day6.rs
@@ -1,39 +1,71 @@
 use crate::solution::Solution;
-use anyhow::Result;
+use anyhow::{bail, Context, Result};
+use num_traits::{One, Zero};
+use std::ops::{Add, Mul};
+use std::str::FromStr;
+
+enum Op {
+    Mul,
+    Add,
+}
+
+impl Op {
+    fn apply<T>(&self, a: T, b: T) -> T
+    where
+        T: Add<Output = T> + Mul<Output = T>,
+    {
+        match self {
+            Self::Add => a + b,
+            Self::Mul => a * b,
+        }
+    }
+
+    fn identity<T: One + Zero>(&self) -> T {
+        match self {
+            Op::Mul => T::one(),
+            Op::Add => T::zero(),
+        }
+    }
+}
+
+impl FromStr for Op {
+    type Err = anyhow::Error;
+
+    fn from_str(op: &str) -> Result<Self> {
+        Ok(match op {
+            "+" => Op::Add,
+            "*" => Op::Mul,
+            _ => bail!("unexpected operation {}", op),
+        })
+    }
+}
 
 #[derive(Default)]
 pub struct Day6;
 impl Solution for Day6 {
     type Part1Output = usize;
     fn part1(&self, input: &str) -> Result<Self::Part1Output> {
-        // let input = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ";
         let mut lines = input.lines().rev();
-        let operations = lines.next().unwrap().split_whitespace().collect::<Vec<_>>();
-        let mut accumulations = operations
-            .iter()
-            .map(|&op| match op {
-                "*" => 1,
-                "+" => 0,
-                _ => panic!(),
-            })
-            .collect::<Vec<usize>>();
+        let operations = lines
+            .next()
+            .context("empty input")?
+            .split_whitespace()
+            .map(Op::from_str)
+            .collect::<Result<Vec<_>>>()?;
+        let mut accumulations = operations.iter().map(Op::identity).collect::<Vec<_>>();
         for line in lines {
             for (i, num) in line.split_whitespace().enumerate() {
-                let num = num.parse::<usize>()?;
-                match operations[i] {
-                    "*" => accumulations[i] *= num,
-                    "+" => accumulations[i] += num,
-                    _ => panic!("unknown operation"),
-                }
+                let num = num.parse()?;
+                let acc = &mut accumulations[i];
+                *acc = operations[i].apply(*acc, num);
             }
         }
-        let total = accumulations.iter().sum::<usize>();
+        let total = accumulations.iter().sum();
         Ok(total)
     }
 
     type Part2Output = usize;
     fn part2(&self, input: &str) -> Result<Self::Part2Output> {
-        // let input = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ";
         let row_length = input.find('\n').unwrap_or(input.len());
         let col_length = input.lines().count();
         let chars: Vec<_> = input
@@ -48,17 +80,11 @@ impl Solution for Day6 {
         let operations = input
             .lines()
             .last()
-            .unwrap()
+            .context("empty input")?
             .split_whitespace()
-            .collect::<Vec<_>>();
-        let mut accumulations = operations
-            .iter()
-            .map(|&op| match op {
-                "*" => 1,
-                "+" => 0,
-                _ => panic!(),
-            })
-            .collect::<Vec<usize>>();
+            .map(Op::from_str)
+            .collect::<Result<Vec<_>>>()?;
+        let mut accumulations = operations.iter().map(Op::identity).collect::<Vec<_>>();
 
         let mut i = 0;
         for column in columns {
@@ -66,15 +92,12 @@ impl Solution for Day6 {
             if column.is_empty() {
                 i += 1;
             } else {
-                let num = column.parse::<usize>()?;
-                match operations[i] {
-                    "*" => accumulations[i] *= num,
-                    "+" => accumulations[i] += num,
-                    _ => panic!("unknown operation"),
-                }
+                let num = column.parse()?;
+                let acc = &mut accumulations[i];
+                *acc = operations[i].apply(*acc, num);
             }
         }
-        let total = accumulations.iter().sum::<usize>();
+        let total = accumulations.iter().sum();
         Ok(total)
     }
 }
